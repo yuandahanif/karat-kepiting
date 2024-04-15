701c0d23b9a351aead6a419efc566fe242afd443
yuanda@laptop <34027873+yuandahanif@users.noreply.github.com>
2024-04-15 19:59:23 +0700

    use enum

--- defining_an_enum/src/main.rs
+++ defining_an_enum/src/main.rs
@@ -1,16 +1,11 @@
 enum IPAddrKind {
-    V4(String),
+    V4(u8, u8, u8, u8),
     V6(String),
 }
 
-struct IpAddr {
-    kind: IPAddrKind,
-    address: String,
-}
-
 fn main() {
-    let v4 = IPAddrKind::V4;
-    let v6 = IPAddrKind::V6;
+    let v4 = IPAddrKind::V4(127, 0, 0, 1);
+    let v6 = IPAddrKind::V6(String::from("::1"));
 
     println!("Hello, world!");
 }
