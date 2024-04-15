53b0485fba12532d34da5d27914db8752a74dcd1
yuanda@laptop <34027873+yuandahanif@users.noreply.github.com>
2024-04-15 19:48:55 +0700

    5.3 method syntax

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -5,12 +5,28 @@ fn main() {
         width: dbg!(rect1.0 * scale), // dbg! return the ownership so width will be valid
         height: rect1.1,
     };
+    let rect3 = Rectangle {
+        width: 10,
+        height: 12,
+    };
 
     println!("{:#?}", rect2); // :? for debug print, add # to format the output
+
     println!(
         "The area of the rectangle is {} square pixels.",
         area_3(&rect2)
     );
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect2.area()
+    );
+
+    println!("can rect 2 hold rect 3? {}", rect2.can_hold(&rect3));
+    println!("can rect 3 hold rect 2? {}", rect3.can_hold(&rect2));
+
+    let sq = Rectangle::square(442);
+    dbg!(&sq);
 
     dbg!(&rect2); // debug print
 }
@@ -34,4 +50,28 @@ struct Rectangle {
 
 fn area_3(rect: &Rectangle) -> u32 {
     area_2((rect.width, rect.height))
+}
+
+// itter 4
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, rect: &Rectangle) -> bool {
+        let outer_area = self.area();
+        let inner_area = rect.area();
+        inner_area <= outer_area
+    }
+}
+
+// itter 5
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        // static??
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
