7bd219fbec8caabb418b942a2d9fdf5fc0ee9a13
yuanda@laptop <34027873+yuandahanif@users.noreply.github.com>
2024-04-15 22:26:32 +0700

    6.1 defining an enum

--- defining_an_enum/src/main.rs
+++ defining_an_enum/src/main.rs
@@ -11,11 +11,20 @@ impl IPAddrKind {
 }
 
 fn main() {
+    let nullable_url: Option<String> = Some(String::from("localhost")); // this is also enum, with nullable value
     let v4 = IPAddrKind::V4(127, 0, 0, 1);
     let v6 = IPAddrKind::V6(String::from("::1"));
 
     v4.dbg();
     v6.dbg();
 
-    println!("Hello, world!");
+    // type check
+    match nullable_url {
+        None => {
+            println!("there is no url");
+        }
+        Some(url) => {
+            println!("the url is {}", url);
+        }
+    }
 }
