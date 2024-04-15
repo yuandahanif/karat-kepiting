4a38bbd5815e5bfdf9716c94488a358bacbdf08d
yuanda@laptop <34027873+yuandahanif@users.noreply.github.com>
2024-04-15 22:14:52 +0700

    enum method

--- defining_an_enum/src/main.rs
+++ defining_an_enum/src/main.rs
@@ -1,11 +1,21 @@
+#[derive(Debug)]
 enum IPAddrKind {
     V4(u8, u8, u8, u8),
     V6(String),
 }
 
+impl IPAddrKind {
+    fn dbg(&self) {
+        dbg!(&self);
+    }
+}
+
 fn main() {
     let v4 = IPAddrKind::V4(127, 0, 0, 1);
     let v6 = IPAddrKind::V6(String::from("::1"));
+
+    v4.dbg();
+    v6.dbg();
 
     println!("Hello, world!");
 }
