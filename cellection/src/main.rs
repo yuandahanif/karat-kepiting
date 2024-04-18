66539967fbf47b4563f345f23274cca2e9a7cb1e
yuanda@yume <34027873+yuandahanif@users.noreply.github.com>
2024-04-18 20:14:35 +0700

    8.2 strings

--- cellection/src/main.rs
+++ cellection/src/main.rs
@@ -52,4 +52,35 @@ fn main() {
     })];
 
     println!("all other plants {:?}", plants);
+
+    strings();
+}
+
+fn strings() {
+    let data = "initial content";
+
+    let s = String::new();
+    let s = String::from("initial content");
+    let s = data.to_string();
+    let mut s = "initial_content".to_string();
+
+    // encode as UTF-8
+    let hello = String::from("안녕하세요");
+    let hello = String::from("こんにちは");
+
+    s.push(' ');
+    s.push_str(&hello.clone());
+
+    println!("{:?}", s);
+
+    let hello3 = format!("{hello} {hello} {hello}");
+    println!("{:?}", hello3);
+
+    for ch in hello.chars() {
+        print!("{ch} - ");
+    }
+
+    for byte in hello.bytes() {
+        print!("{byte} - ");
+    }
 }
