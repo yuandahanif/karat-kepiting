679c405e16fc4fbad272f1dd0bcaeecbabab8912
yuanda@yume <34027873+yuandahanif@users.noreply.github.com>
2024-04-24 14:18:58 +0700

    Returning a Result Instead of Calling panic!

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -8,16 +8,21 @@ pub mod config {
     }
 
     impl Config {
-        pub fn new(args: &[String]) -> Config {
-            Config {
+        fn build(args: &[String]) -> Result<Config, &'static str> {
+            if args.len() < 3 {
+                return Err("not enough arguments!");
+            }
+
+            Ok(Config {
                 query: args[1].clone(),
                 file_path: args[2].clone(),
-            }
+            })
         }
     }
 
-    pub fn parse_config() -> Config {
+    pub fn parse_config() -> Result<Config, &'static str> {
         let args: Vec<String> = env::args().collect();
-        Config::new(&args)
+
+        Config::build(&args)
     }
 }
