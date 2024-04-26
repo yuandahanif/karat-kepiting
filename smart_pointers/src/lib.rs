63460b45f51fa77a0c213ee00934e68d2d6584f3
yuanda@yume <34027873+yuandahanif@users.noreply.github.com>
2024-04-27 00:47:55 +0700

    15.2. Treating Smart Pointers Like Regular References with the Deref Trait

--- smart_pointers/src/lib.rs
+++ smart_pointers/src/lib.rs
@@ -6,3 +6,25 @@ pub mod cons {
         Nil,
     }
 }
+
+// my_std sound weird
+pub mod my_std {
+    use std::ops::Deref;
+
+    pub struct MyBox<T>(T);
+
+    impl<T> MyBox<T> {
+        pub fn new(inp: T) -> MyBox<T> {
+            MyBox(inp)
+        }
+    }
+
+    impl<T> Deref for MyBox<T> {
+        type Target = T;
+    
+        fn deref(&self) -> &Self::Target {
+            &self.0
+        }
+    }
+}
+
--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,6 +1,37 @@
 use smart_pointers::cons::List::{Cons, Nil};
+use smart_pointers::my_std;
 
 fn main() {
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
     println!("Hello, world! {:?}", list);
+
+    deref();
+
+    let x = 5;
+    let y = my_std::MyBox::new(x);
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let m = my_std::MyBox::new(String::from("Rust"));
+    hello(&m);
+    hello(&(*m)[..]); // explicit; without deref coercion
+}
+
+fn deref() {
+    let x = 5;
+    let y = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(&5, y);
+    assert_eq!(5, *y);
+
+    let y = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(Box::new(5), y);
+    assert_eq!(5, *y);
+}
+
+fn hello(name: &str) {
+    println!("Hello, {name}!");
 }
