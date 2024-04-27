734b14321813b4fe5e8638a54db97e13116f0126
yuanda@yume <34027873+yuandahanif@users.noreply.github.com>
2024-04-27 20:30:13 +0700

    Channels and Ownership Transference

--- fearless_concurrency/src/main.rs
+++ fearless_concurrency/src/main.rs
@@ -1,8 +1,10 @@
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 
 fn main() {
     let v = vec![1, 2, 3];
+    let (tx, rx) = mpsc::channel::<String>();
 
     let handle = thread::spawn(|| {
         for i in 1..10 {
@@ -14,11 +16,40 @@ fn main() {
     let handle = thread::spawn(move || {
         println!("Here's a vector: {:?}", v);
     });
-
     handle.join().unwrap();
+
+    let handle = thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
+
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
 
     // for i in 1..5 {
     //     println!("hi number {} from the main thread!", i);
     //     thread::sleep(Duration::from_millis(1));
     // }
+    pause_itter();
+}
+
+fn pause_itter() {
+    let (tx, rc) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+        }
+    });
+
+    for recived in rc {
+        println!("Got: {}", recived);
+    }
 }
